566e99d21d9a0b3991ec44aaf9f5793d7363529e
yegor-gondhal <ygondhal@gmail.com>
2026-03-16 17:47:45 -0400

    Update

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 driver = webdriver.Chrome()
